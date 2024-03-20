4d9b1d1a51807be8eb1e5855fb4b0312c9142fd5
jyoti ranjan sharma <jyotiranjansharma11@gmail.com>
2024-03-20 17:52:05 +0530

    feat(route): update context data

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -16,17 +16,21 @@ const formatMessage = (message: VercelChatMessage) => {
 
 const DATA = [
     new Document({
-        pageContent: `Jyoti is an Astronaut and loves space travelling. He has been to the Moon, Mars and Saturn. `,
+        pageContent: `Jyotiranjan is a Software Engineer specializing in
+        React.js and Next.js development and Machine Learning. With Next.js
+        he builds fast and intuitive web applications, while
+        delving into Machine Learning to drive innovation and
+        solve complex challenges. He previously worked at Wipro,
+        TechMahindra and on Fortune 500 sites.`,
         metadata: { source: 1, title: "Jyoti's Space History" },
     }),
     new Document({
-        pageContent: `He have been living on Earth since 25 years now and would like settle here for a while.
-        Until his next appointment to his next exploration to 'Proxima Centauri b' super planet`,
+        pageContent: `He have been living on Earth since a few years now and would like settle here for a while. During his stay on planet Earth,
+        he would like to work and take humanity forward and build systems that would make space travel possible.`,
         metadata: { source: 2, title: "Jyoti's next exploration" },
     }),
     new Document({
-        pageContent: `Jyoti is now working as a Senior Software Engineer and plans to automate every human activity with LLMs 
-        and help humans find life on other planets.`,
+        pageContent: `Jyoti is building interesting frontend and machine learning applications which would help build things that takes the human race forward. And you may contact him on Twitter (https://twitter.com/JyotiRanjanSha9) or LinkedIn (https://www.linkedin.com/in/jrsharma11/)`,
         metadata: { source: 3, title: "Jyoti's current life" },
     }),
 ];
@@ -35,7 +39,7 @@ const createContext = async () =>
     await MemoryVectorStore.fromDocuments(DATA, new OpenAIEmbeddings());
 
 const model = new ChatOpenAI({
-    temperature: 0,
+    temperature: 0.6,
     openAIApiKey: process.env.OPENAI_API_KEY,
 });
 
@@ -48,7 +52,7 @@ export async function POST(req: NextRequest) {
     const context = await createContext();
     const results = await context.similaritySearch(currentMessageContent, 1);
 
-    const TEMPLATE = `You are Jarvis from Iron Man and you are Jyoti's assistant.
+    const TEMPLATE = `You are Jarvis from Iron Man and you are Jyoti's assistant. Jyoti is a Man.
     All responses must be extremely verbose and in tech dialect and must follow the context.
     You are responding to 3rd party users. Jyoti never speaks to them. Only you speak to Jyoti's users.
     Never answer anything beyond the given context. Revolve around Jyoti, if they ask further details, tell them to ask Jyoti directly.
